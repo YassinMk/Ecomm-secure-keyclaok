a725a05c9a0ba104954640749498b2c4c68949e1
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-12-16 00:29:38 +0100

    add microservice inventory

---
+++ ecom-backend-keycloak-main/inventory-service/src/main/java/com/ecom/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,29 @@
+package com.ecom.inventoryservice;
+
+import com.ecom.inventoryservice.entities.Product;
+import com.ecom.inventoryservice.repositories.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.UUID;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+
+	@Bean
+	CommandLineRunner start(ProductRepository productRepository) {
+		return args -> {
+			productRepository.save(Product.builder().id("P01").name("Laptop").price(1000).quantity(10).build());
+			productRepository.save(Product.builder().id("P02").name("Smartphone").price(500).quantity(20).build());
+			productRepository.save(Product.builder().id("P03").name("Tablet").price(300).quantity(30).build());
+		};
+	}
+
+}
---
+++ ecom-backend-keycloak-main/inventory-service/src/main/java/com/ecom/inventoryservice/web/ProductRestController.java
@@ -0,0 +1,33 @@
+package com.ecom.inventoryservice.web;
+
+import com.ecom.inventoryservice.entities.Product;
+import com.ecom.inventoryservice.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api")
+public class ProductRestController {
+    private final ProductRepository productRepository;
+
+    @GetMapping("/products")
+//    @PreAuthorize("hasAuthority('ADMIN')")
+    public List<Product> productList(){
+        return productRepository.findAll();
+    }
+    @GetMapping("/products/{id}")
+    //@PreAuthorize("hasAuthority('USER')")
+    public Product productById(@PathVariable String id){
+        return productRepository.findById(id).get();
+    }
+    @GetMapping("/auth")
+    public Authentication authentication(Authentication authentication){
+        return authentication;
+    }
+
+}
